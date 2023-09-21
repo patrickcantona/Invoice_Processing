55eee713968bbf2b3203546f4d1e348094da52e1
patrickcantona <ariablue92@gmail.com>
2023-09-21 15:58:20 +0200

    modify replace_text and add gitignore

--- replace_text.py
+++ replace_text.py
@@ -1,27 +1,51 @@
 import fitz
 import sys
+import os
+from detect_amount import *
+import warnings
 
-def open_pdf(input_pdf, new_text, old_text, output_pdf):
+# Désactiver tous les avertissements (warnings)
+warnings.filterwarnings("ignore")
+
+processed_invoices_folder = "processed_invoices/"
+
+def process_pdf(input_pdf):
     doc = fitz.open(input_pdf)
 
     page = doc[0] 
+
+    file_name = os.path.basename(input_pdf)
+    output_filename =  "processed_" + file_name
+    output_pdf = os.path.join(processed_invoices_folder, output_filename)
+
+    image_path =  convert_pdf_to_images(input_pdf)
+    answer =  extract_total_amount(image_path)
+    
+    total_mount = extract_and_convert_number(answer)
+
+    total_amount_with_margin = calculate_margin(total_mount)
+
+    total_amount_with_margin_text = process_answer(answer, total_amount_with_margin )
+
+    print("The total amount detected is : " , answer)
+    print("The total amount converted is : " , total_mount)
+    print("The total amount with margin is : " , total_amount_with_margin_text)
+
     # list of rectangles where to replace
 
-    hits = page.search_for(old_text)  
+    hits = page.search_for(answer)  
     for rect in hits:
         point = (rect[0], rect[-1])
 
         page.draw_rect(rect, fill=(1,), color=(1, 1, 1))
-        page.insert_text(point, new_text, fontsize=11, fontname='helv')
+        page.insert_text(point, total_amount_with_margin_text, fontsize=11, fontname='helv')
 
+    
     doc.save(output_pdf)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python your_script.py input_pdf new_text old_text output_pdf")
+    if len(sys.argv) != 2:
+        print("Usage: python your_script.py input_pdf")
     else:
         input_pdf = sys.argv[1]
-        new_text = sys.argv[2]
-        old_text = sys.argv[3]
-        output_pdf = sys.argv[4]
-        open_pdf(input_pdf, new_text, old_text, output_pdf)
+        process_pdf(input_pdf)
